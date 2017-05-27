efa25622a96f2bb5472182247e08c051e6162164
Hayabusa <yuuta.keiko.k@live.jp>
2017-05-27 19:04:21 +0900

    [Release] 1.0.0
    - 再生中の曲情報を取得してウィンドウに表示する機能を追加
    - ボタンをクリックすると取得した曲情報をクリップボードにコピーする機能を追加

---
+++ Form1.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using iTunesLib;
+
+
+namespace GetNowPlayingfromiTunes
+{
+    public partial class Form1 : Form
+    {
+        iTunesApp app = new iTunesApp();
+        
+        string copytxt;
+        public Form1()
+        {
+            InitializeComponent();
+            
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+        private void button2_Click(object sender, EventArgs e)
+        {
+            
+            
+        }
+
+        private void button2_Click_1(object sender, EventArgs e)
+        {
+            IITTrack track = app.CurrentTrack;
+            label4.Visible = true;
+            label4.Text = track.Name;
+            label5.Visible = true;
+            label5.Text = track.Artist;
+            label6.Visible = true;
+            label6.Text = track.Album;
+            System.Diagnostics.Debug.WriteLine("This is a message for Debugging.");
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            IITTrack track = app.CurrentTrack;
+            copytxt = "#nowplaying " + track.Name + " / " + track.Artist;
+            Clipboard.SetText(copytxt);
+        }
+    }
+}
